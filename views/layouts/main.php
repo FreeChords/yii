c545a76bb22962d2e8b964f882d74b6711c077cc
DiegoCardenasV <diego.cardenasv@alumnos.uv.cl>
2015-10-16 03:49:08 -0300

    Votar
    Vista de Pestaña de Votacion, Solo la Vista

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,7 +38,7 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Votar', 'url' => ['/site/contact']],
             
         ],
     ]);
